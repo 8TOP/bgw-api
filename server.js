9d2b5d9720fa2f86318445bc555734f7932fcb80
Joshua Milus <redrooster9@gmail.com>
2019-11-23 15:01:46 -0800

    port changes

--- server.js
+++ server.js
@@ -6,25 +6,28 @@ const create = require('./controllers/create');
 const knex = require('knex');
 
 const port = (process.env.PORT ? proess.env.PORT : 3000);
-let dbConnection = {};
-if (port === 3000) {
-    console.log("backend running on localhost");
-    dbConnection = {
-        host: '127.0.0.1',
-        user: 'Joshua',
-        password: '',
-        database: 'big-giant-wall-db'
-    }
-} else {
-    dbConnection = {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
-    }
-}
+// let dbConnection = {};
+// if (port === 3000) {
+//     console.log("backend running on localhost");
+//     dbConnection = {
+//         host: '127.0.0.1',
+//         user: 'Joshua',
+//         password: '',
+//         database: 'big-giant-wall-db'
+//     }
+// } else {
+//     dbConnection = {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: true
+//     }
+// }
 
 const db = knex({
     client: 'pg',
-    connection: dbConnection
+    connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: true
+    }
 });
 
 const app = express();
@@ -36,8 +39,8 @@ app.get('/', (req, res) => { res.send("wall backend connected.") });
 app.get('/zoneX/:x/zoneY/:y', (req, res) => load.loadHandler(req, res, db));
 app.post('/create', (req, res) => create.createHandler(req, res, db));
 
-app.listen(port, () => {
-    console.log(`server operational on port ${port}`);
+app.listen(process.env.PORT, () => {
+    console.log(`server operational on port ${process.env.PORT}`);
 })
 
 /*
